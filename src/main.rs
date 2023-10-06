857fc2c68074ee9cebf163f867d9e76145581408
Yeison Montoya <yeison.montoya@pragma.com.co>
2023-10-06 17:05:23 -0500

    Y.Montoya: First variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let minima: i8 = -15;
+    let mut maxima: u8 = 40;
+
+    maxima = maxima + 1;
+
+    println!(
+        "En mi ciudad la minima fue de {} y la maxima fue de {}",
+        minima, maxima
+    );
 }
