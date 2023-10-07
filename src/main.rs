5907b8238df2ec51cc5b792efb241205cf5094e7
Yeison Montoya <yeison.montoya@pragma.com.co>
2023-10-06 22:45:15 -0500

    Y.Montoya: Fibbonaci ejercicio

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
 fn main() {
-    // dos números
-    let num_1 = 120;
-    let num_2 = 321;
+    let mut fibo_user = String::new();
+    println!("Escribe el número Fibonnaci que necesites:");
+    std::io::stdin().read_line(&mut fibo_user).unwrap();
 
-    let sum = num_1 + num_2;
+    let fibo_user_int = fibo_user.trim().parse().unwrap();
 
-    loop {
-        println!("Escribe la sume de {} y {}", num_1, num_2);
+    println!(
+        "Numero fibo de {} es {}",
+        fibo_user_int,
+        get_fibo_number(fibo_user_int)
+    );
+}
 
-        let mut sum_user = String::new();
+fn get_fibo_number(n: i64) -> i64 {
+    let mut fibo_number: Vec<i64> = Vec::new();
+    let mut i = 0;
 
-        std::io::stdin().read_line(&mut sum_user).unwrap();
-
-        let sum_user_int: i32 = sum_user.trim().parse().unwrap();
-
-        if sum_user_int == sum {
-            println!("La suma es correcta.");
-            break;
+    while i <= n {
+        if i == 0 {
+            fibo_number.push(1);
+        } else if i == 1 || i == 2 {
+            fibo_number.push(i);
         } else {
-            println!("La suma es errada");
+            let i_1 = i - 1;
+            let i_2 = i - 2;
+
+            let new_number = fibo_number[i_1 as usize] + fibo_number[i_2 as usize];
+
+            fibo_number.push(new_number);
         }
+        i += 1;
     }
+
+    return fibo_number[n as usize];
 }
