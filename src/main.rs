066ebf7b798d9670d443667e631fc4ce8dcfe5af
Yeison Montoya <yeison.montoya@pragma.com.co>
2023-10-06 20:56:21 -0500

    Y.Montoya: el If de toda la vida

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 fn main() {
-    println!("Introduce tu nombre:");
-    let mut name: String = String::new();
-    std::io::stdin().read_line(&mut name).unwrap();
-    name = name.trim().to_string();
-
-    // obtener la edad y convertir esa edad a un número
-    println!("Introduce tu edad:");
+    println!("Introduce tu edad: ");
     let mut age: String = String::new();
+
+    // Tomar edad
     std::io::stdin().read_line(&mut age).unwrap();
 
-    // transformamos la edad en número y quitamos saltos y espacio con trim()
+    // Transformar Edad
     let age_int: u8 = age.trim().parse().unwrap();
 
-    println!("Hola {} de {} años", name, age_int);
+    if age_int >= 18 {
+        println!("Puedes entrar a la discoteca");
+    } else {
+        println!("No puedes entrar a la discoteca")
+    }
+
+    let mut i: u32 = 30;
+
+    i += 1;
+
+    println!("I es: {}", i)
 }
