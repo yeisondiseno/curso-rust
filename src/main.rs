78c863c6b9876b2794ae78d50249fb535fda8924
Yeison Montoya <yeison.montoya@pragma.com.co>
2023-11-14 22:49:34 -0500

    Y.Montoya: Calculator completed

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,81 @@
-fn main() {
-    let mut fibo_user = String::new();
-    println!("Escribe el número Fibonnaci que necesites:");
-    std::io::stdin().read_line(&mut fibo_user).unwrap();
+use regex::Regex;
+use std::io;
 
-    let fibo_user_int = fibo_user.trim().parse().unwrap();
+// *Note: The packages are obtaining from https://crates.io/
 
-    println!(
-        "Numero fibo de {} es {}",
-        fibo_user_int,
-        get_fibo_number(fibo_user_int)
-    );
+// function
+fn multiply(a: i64, b: i64) -> i64 {
+    return a * b;
 }
 
-fn get_fibo_number(n: i64) -> i64 {
-    let mut fibo_number: Vec<i64> = Vec::new();
-    let mut i = 0;
+fn division(a: i64, b: i64) -> i64 {
+    return a / b;
+}
 
-    while i <= n {
-        if i <= 2 {
-            fibo_number.push(i);
-        } else {
-            let i_1 = i - 1;
-            let i_2 = i - 2;
+fn sum(a: i64, b: i64) -> i64 {
+    return a + b;
+}
 
-            let new_number = fibo_number[i_1 as usize] + fibo_number[i_2 as usize];
+fn rest(a: i64, b: i64) -> i64 {
+    return a - b;
+}
 
-            fibo_number.push(new_number);
+fn create_regex(expression: &str) -> Regex {
+    let re = format!(r"(\d+)\s?\{}\s?(\d+)", expression);
+    return Regex::new(&re).unwrap();
+}
+
+fn create_expression(
+    regex: Regex,
+    operation_function: impl Fn(i64, i64) -> i64,
+    int_expression: String,
+) -> String {
+    loop {
+        // Operations
+        let caps = regex.captures(int_expression.as_str());
+
+        if caps.is_none() {
+            return int_expression;
         }
-        i += 1;
-    }
 
-    return fibo_number[n as usize];
+        let caps = caps.unwrap();
+        let cap_expression = caps.get(0).unwrap().as_str();
+        let left_value: i64 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i64 = caps.get(2).unwrap().as_str().parse().unwrap();
+        let result = operation_function(left_value, right_value);
+
+        return int_expression.replace(cap_expression, &result.to_string());
+    }
 }
+
+fn main() {
+    // Regex
+    let re_add = create_regex("+");
+    let re_res = create_regex("-");
+    let re_mul = create_regex("*");
+    let re_div = create_regex("/");
+
+    // User Data
+    println!("Introduce tu expresión");
+    let mut expression: String = String::new();
+    io::stdin().read_line(&mut expression).unwrap();
+
+    // multiplication
+    expression = create_expression(re_mul, multiply, expression);
+
+    // Division
+    expression = create_expression(re_div, division, expression);
+
+    // sum
+    expression = create_expression(re_add, sum, expression);
+
+    // Rest
+    expression = create_expression(re_res, rest, expression);
+
+    // Result
+    println!("resultado: {expression}")
+}
+
+// In Rust the type null doesn't exist.
+// Option<T> = optional type.
+// Result<T,E> = T Type E Error.
